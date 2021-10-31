a0f36eab63b1c0c83a25b6bde051d843ab83050c
LatchedMicrobe0 <rodrigo.pontes.oliveira.lima@gmail.com>
2021-10-31 20:47:01 -0300

    Testando bot de moedas

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import json
 
 while(True):
     n = 0
+    coin_input = input("Insert your prefered coin: ")
     user_input = input("Do you want to quit? y/n:")
     if user_input == 'y' or  user_input == 'n':
         if user_input != 'y':
             while n < 7:
-                cotacao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+                cotacao = requests.get("https://economia.awesomeapi.com.br/json/all")
                 cotacao = cotacao.json()
-                cotacao_btc = cotacao["BTCBRL"]["bid"]
-                print(cotacao_btc)
+                cotacao_coin = cotacao[coin_input]["bid"]
+                print(cotacao_coin)
                 time.sleep(1)
                 n += 1
         else:
